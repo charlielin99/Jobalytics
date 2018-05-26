458acb81f025420fd1259fd83bda613585d0325f
Charlie Lin <hlin266@uwo.ca>
2018-05-26 16:26:00 -0400

    Added local storage

--- frontend/js/jobmatch.js
+++ frontend/js/jobmatch.js
@@ -1,6 +1,4 @@
 function populateRes(){
     var str = document.getElementById("inputsection").value;
-    var sr = str.split(" ");
-    console.log('check0')
-    var file = new File(sr, "hi.txt", {type: "text/plain"});
+    localStorage.setItem("descr", str);
 }
