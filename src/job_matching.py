182aafce029299471347bc8c22f19c4cfd1b8e91
Calvin Woo <oow.nivlac@gmail.com>
2018-05-26 13:56:01 -0400

    algos almost done

--- src/job_matching.py
+++ src/job_matching.py
@@ -16,10 +16,8 @@ The flow is:
 
 '''
 
-job_desc = "You must have experience with two of the following: HMTL, JavaScript and CSS, and typical UI libraries such as Angular IO, Bootstrap, etc. Java and typical Java libraries such as Spring Boot, JPA/Hibernate, JHipster, etc. Analyzing and querying relational databases Experience with the following is beneficial: Linux Common development tools, such as Maven, Git, Jenkins, etc. Any RAD or Agile development methodology"
-resume = "Muyun(Vicky) Lyum3lyu@edu.uwaterloo.ca 226-201-4422 2A Computer Engineering | University of Waterloo ID#20664422 Summary of Qualifications Experienced with C++, JAVA, strengthened by projects and related courses Experienced with Android application development and object-oriented programming in work place and related courses Familiar with Python, MySQL and PHP by self-stud"
-shit_words = ['experience','Experienced','Proficient','engineering','efficient','Qualifications','Engineering','courses','work','Summary','University','university','University of Waterloo']
-
+job_desc = ""
+resume = ""
 def analyzer(job_desc):
 	shordy = keywords(job_desc, threshold = 0.15)
 	return [ a for a in shordy.keys() ]
