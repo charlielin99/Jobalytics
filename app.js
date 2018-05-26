00d09f9b5051d643ea664d8be85defee98f7f309
Charlie Lin <hlin266@uwo.ca>
2018-05-26 01:36:07 -0400

    Testing npm unfluff

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var express = require("express");
 var app = express();
+extractor = require('unfluff');
+var fs = require('fs');
+data = extractor(fs.readFileSync('https://stackoverflow.com/questions/28908547/node-js-does-not-recognise-the-url-in-the-unfluff-module'));
 
-app.get("foobar", function(req,res){
-
-
+/*
+fs.readFile('test.html', function(err, html){
+    var data = extractor(html);
+    console.log(data);
 });
+*/
+
+
+app.get("/hello", function(req,res){
+	res.send(data.text);
+});
+
+app.listen(3000, () => {
+        console.log("good");
+    });
+
+
+
+
