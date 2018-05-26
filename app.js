4f74908f674557e7b2f7fcf47a0c4d3a07366e96
Charlie Lin <hlin266@uwo.ca>
2018-05-26 03:08:30 -0400

    Added job site scrapping

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 var express = require("express");
 var app = express();
-extractor = require('unfluff');
+var extractor = require('unfluff');
 var fs = require('fs');
-data = extractor(fs.readFileSync('https://stackoverflow.com/questions/28908547/node-js-does-not-recognise-the-url-in-the-unfluff-module'));
+var request = require('request');
 
-/*
-fs.readFile('test.html', function(err, html){
-    var data = extractor(html);
-    console.log(data);
+
+var url="https://job-openings.monster.ca/Product-Manager-San-Francisco-San-Francisco-CA-Prolific-Interactive/31/d407fb84-a170-41ae-9fc2-e4e69877524b";
+var destination = fs.createWriteStream('jobDescription.html');
+request (url).pipe(destination);
+
+
+app.get("/test", function(req,res){
+	data = extractor(fs.readFileSync('jobDescription.html'));
+	res.send(data.text)
+	//console.log(data.text)
 });
-*/
 
 
-app.get("/hello", function(req,res){
-	res.send(data.text);
-});
+
+
 
 app.listen(3000, () => {
-        console.log("good");
-    });
+    console.log("good");
+});
 
 
 
