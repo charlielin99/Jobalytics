0ec0ec973a3820737098277958b257873138586e
Charlie Lin <hlin266@uwo.ca>
2018-05-26 16:28:55 -0400

    Linked to job scrapping link js file

--- src/jobscrap.js
+++ src/jobscrap.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var request = require('request');
 
 
-var url="https://job-openings.monster.ca/Product-Manager-San-Francisco-San-Francisco-CA-Prolific-Interactive/31/d407fb84-a170-41ae-9fc2-e4e69877524b";
+var url=localStorage.getItem(“descr”);
 var destination = fs.createWriteStream('jobDescription.html');
 request (url).pipe(destination);
 
